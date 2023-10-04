19296fa33c97a5c39490a4a176caf27324eefecc
Sandeepsoudu <2203a51l86@sru.edu.in>
2023-10-04 14:18:37 +0530

    day-7

--- DAY-7/subsetssum.py
+++ DAY-7/subsetssum.py
@@ -1,6 +1,6 @@
 def subset_sum(arr, n, target, current_subset=[]):
     if target == 0:
-        print("Subset with the given sum:", current_subset)
+        print(current_subset)
         return
     if n == 0 and target != 0:
         return
@@ -11,7 +11,7 @@ def subset_sum(arr, n, target, current_subset=[]):
     subset_sum(arr, n - 1, target - arr[n - 1], include_current)
     subset_sum(arr, n - 1, target, current_subset)
 
-arr = list(map(int, input().split()))
-target = int(input("Enter the target sum: "))
+arr = list(map(int, input("enter elements").split()))
+target = int(input("enter target sum"))
 n = len(arr)
 subset_sum(arr, n, target)
