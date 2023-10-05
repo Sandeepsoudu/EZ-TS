5e540782693aad740219e3f51efa193433450fec
Sandeepsoudu <2203a51l86@sru.edu.in>
2023-10-05 15:00:52 +0530

    day-8

---
+++ Day-8/inversion.py
@@ -0,0 +1,9 @@
+def inversion(a):
+    count=0
+    for i in range(len(a)):
+        for j in range(i+1,len(a)):
+            if a[i]>a[j]:
+                count+=1
+    return count
+a=list(map(int,input("enter the numbers").split()))
+print(inversion(a))
