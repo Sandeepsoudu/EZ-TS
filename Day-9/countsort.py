9dc8ac246c32dca05c6637078a441ce4a4a5c775
Sandeepsoudu <2203a51l86@sru.edu.in>
2023-10-06 10:33:35 +0530

    changes

---
+++ Day-9/countsort.py
@@ -0,0 +1,17 @@
+def count_sort(lst):
+    s=len(lst)
+    count_arr=[0]*s
+    res_arr=[0]*s
+    for i in range(s):
+        count_arr[lst[i]]+=1
+    for i in range(1,s):
+        count_arr[i]+=count_arr[i-1]
+    n=s-1
+    while n>=0:
+        res_arr[count_arr[lst[n]]-1]=lst[n]
+        count_arr[lst[n]]-=1
+        n-=1
+    for i in range(n):
+        print(res_arr[i])
+lst=list(map(int,input().split()))
+count_sort(lst)
