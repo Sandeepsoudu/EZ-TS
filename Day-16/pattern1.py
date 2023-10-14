a4e655748afc475800a30ed22ae844fd96a9170c
Sandeepsoudu <2203a51l86@sru.edu.in>
2023-10-14 14:31:25 +0530

    changes

--- Day-16/pattern1.py
+++ Day-16/pattern1.py
@@ -3,7 +3,7 @@ col=9
 for i in range(row):
     for j in range(col):
         if i==j or i+j==9-1:
-            print("*",end=" ")
-        else:
             print(" ",end=" ")
+        else:
+            print("*",end=" ")
     print()
