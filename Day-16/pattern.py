2d240237db55f0ce09cae32a3542eb8ec6aa9688
Sandeepsoudu <2203a51l86@sru.edu.in>
2023-10-16 09:42:23 +0530

    changes

--- Day-16/pattern.py
+++ Day-16/pattern.py
@@ -1,12 +1,12 @@
-n=3
+n=4
 col=2*n-1
 start,end=n-1,n-1
 for i in range(0,n,1):
     for j in range(col):
         if j>=start and j<=end: 
-            print("*",end=" ")
-        else:
             print(" ",end=" ")
+        else:
+            print(j,end=" ")
     print()
     start-=1
     end+=1
--- Day-16/pattern1.py
+++ Day-16/pattern1.py
@@ -1,8 +1,8 @@
-row=9
-col=9
+row=4
+col=4
 for i in range(row):
     for j in range(col):
-        if i==j or i+j==9-1:
+        if i==j or i+j==4-1:
             print(" ",end=" ")
         else:
             print("*",end=" ")
